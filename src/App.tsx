c971f58c9013d351bfadff802ed96e99f332e25a
Mateusz Wieczorek <mateusz.wu@gmail.com>
2021-08-29 21:40:20 +0200

    configure react chrome extension, and add simple dom evaluator

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { DOMMessageResponse, DOMMessage } from './types'
 
 function App() {
+  const [title, setTitle] = React.useState(8);
+  const buttonHandler = () => {
+    chrome.tabs && chrome.tabs.query({
+      active: true,
+      currentWindow: true
+    }, tabs => {
+    /**
+     * Sends a single message to the content script(s) in the specified tab,
+     * with an optional callback to run when a response is sent back.
+     *
+     * The runtime.onMessage event is fired in each content script running
+     * in the specified tab for the current extension.
+     */
+      chrome.tabs.sendMessage(
+      tabs[0].id || 0,
+      { type: 'GET_DOM' } as DOMMessage,
+      (response: DOMMessageResponse) => {
+        setTitle(response.test)
+      });
+    });
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={buttonHandler}>LCick</button>
+      <div>
+      {title}
+      </div>
     </div>
   );
 }
---
+++ src/types.ts
@@ -0,0 +1,8 @@
+export type DOMMessage = {
+  type: 'GET_DOM'
+}
+
+export type DOMMessageResponse = {
+  title: string;
+  test: number;
+}
